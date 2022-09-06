7c992afc5f31a54de23aeb9ad34c7992fa7659ed
Alish <abdyrazakovv0@gmail.com>
2022-09-06 17:14:08 +0000

    Import BrowsRouter, Rout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom';
 import logo_pizza from '../assets/img/pizza-logo.svg'
 import Button from './Button';
 
 function Header() {
-    return(
-        <div className="header">
-        <div className="container">
+  return (
+    <div className="header">
+      <div className="container">
+        <Link to='/'>
           <div className="header__logo">
             <img width="38" src={logo_pizza} alt="Pizza logo" />
             <div>
-              <h1>React Pizza</h1>
+                <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
             </div>
           </div>
-          <div className="header__cart">
+        </Link>
+        <div className="header__cart">
+          <Link to='/cart'>
             <Button className="button--cart">
-            <span>520 ₽</span>
+              <span>520 ₽</span>
               <div className="button__delimiter"></div>
               <svg
                 width="18"
@@ -47,12 +51,13 @@ function Header() {
                 />
               </svg>
               <span>3</span>
-            </Button> 
-            
-          </div>
+            </Button>
+          </Link>
+
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter as Router,  Routes } from 'react-router-dom';
 import './scss/app.scss';
 
 import App from './App';
+import { Header } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <App />
+    </Router>
   </React.StrictMode>
 );
 
